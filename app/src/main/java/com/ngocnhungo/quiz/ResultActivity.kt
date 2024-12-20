5b19be0770c0e35100c5d6f775d0d1b4e94a76d1
NNgocNhu84 <pth.pnhu2207@gmail.com>
2024-12-20 21:01:18 +0100

    Create ResultActivity.kt

---
+++ app/src/main/java/com/ngocnhungo/quiz/ResultActivity.kt
@@ -0,0 +1,28 @@
+package com.ngocnhungo.quiz
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class ResultActivity : AppCompatActivity() {
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+
+        val score = intent.getIntExtra("SCORE", 0)
+        val scoreTextView: TextView = findViewById(R.id.finalScoreTextView)
+        scoreTextView.text = "Your Score: $score"
+
+        val restartButton: Button = findViewById(R.id.restartButton)
+        restartButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
